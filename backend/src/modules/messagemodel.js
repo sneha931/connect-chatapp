ce87ab7c2cd3c651df74526664f4b8ce62e06247
sneha931 <153498006+sneha931@users.noreply.github.com>
2025-01-06 23:33:46 +0530

    Update commit

--- backend/src/modules/messagemodel.js
+++ backend/src/modules/messagemodel.js
@@ -1,4 +1,4 @@
-const mongoose=require("mongoose");
+import mongoose from "mongoose";
 
 const messageSchema=new mongoose.Schema({
     senderId:{
@@ -20,4 +20,4 @@ const messageSchema=new mongoose.Schema({
 },{timestamps:true})
 
 const Message=mongoose.model("Message",messageSchema)
-module.exports=Message;
+export default Message;
