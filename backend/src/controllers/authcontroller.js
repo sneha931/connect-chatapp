38cb1f385b94ac35751a1eb0b3f5ceaf90f52a28
sneha931 <153498006+sneha931@users.noreply.github.com>
2025-01-07 01:40:17 +0530

    Update authcontroller.js

--- backend/src/controllers/authcontroller.js
+++ backend/src/controllers/authcontroller.js
@@ -65,7 +65,7 @@ export const login=async(req,res)=>{
     }
     catch(err){
         console.log("Error in login controller",err.message);
-        res.status(500).json({message:"Internal server error"})
+        res.status(500).json({message:"Internal server error",error:err.message})
     }
 };
 export const logout=async(req,res)=>{
