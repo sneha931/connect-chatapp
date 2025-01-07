732536b8a9a28b17cdbc14a9e4901b4d47d3570d
sneha931 <153498006+sneha931@users.noreply.github.com>
2025-01-07 16:56:18 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE === "developyment"?"http://localhost:8000/api":"https://connect-chatapp-pb6c.onrender.com/api",
+    baseURL:import.meta.env.MODE === "development"?"http://localhost:8000/api":"https://connect-chatapp-pb6c.onrender.com/api",
     withCredentials:true,
 })
